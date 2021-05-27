fa614a144a06e129d8c90319297564515d1a36ac
Felipe Marinho <felipmarinho@gmail.com>
2021-05-27 09:26:00 -0300

    add route to list category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,14 +3,19 @@ import { Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoriesRepository = new CategoriesRepository();
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const result = categoriesRepository.list();
+  return response.json(result);
 });
 
 export { categoriesRoutes };

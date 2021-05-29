495a0f378e6bd604025edff8a64d37a467e30a29
Felipe Marinho <felipmarinho@gmail.com>
2021-05-29 18:01:14 -0300

    refactore

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoriesRepository } from "../models/cars/repositories/CategoriesRepository";
+import { CreateCategoryService } from "../models/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();

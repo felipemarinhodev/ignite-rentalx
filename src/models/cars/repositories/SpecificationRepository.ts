6e6e697474b13ee453242cec2118afe0f40d9e7b
Felipe Marinho <felipmarinho@gmail.com>
2021-05-29 18:56:47 -0300

    add SpecificationRepositoy and interface

---
+++ src/models/cars/repositories/SpecificationRepository.ts
@@ -0,0 +1,27 @@
+import { Specification } from "../model/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationRepository,
+} from "./ISpecificationRepository";
+
+class SpecificationRepository implements ISpecificationRepository {
+  private specifications: Specification[] = [];
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): Specification {
+    return this.specifications.find(
+      (specification) => specification.name === name
+    );
+  }
+}
+
+export { SpecificationRepository };

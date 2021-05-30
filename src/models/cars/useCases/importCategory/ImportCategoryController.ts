c43ec6b319b3b7e2720a35cff5543c7b42425080
Felipe Marinho <felipmarinho@gmail.com>
2021-05-30 18:23:39 -0300

    remove console.log

--- src/models/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/models/cars/useCases/importCategory/ImportCategoryController.ts
@@ -6,9 +6,7 @@ class ImportCategoryController {
   constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
   handle(request: Request, response: Response): Response {
     const { file } = request;
-    console.log(file);
     this.importCategoryUseCase.execute({ file });
-
     return response.send();
   }
 }

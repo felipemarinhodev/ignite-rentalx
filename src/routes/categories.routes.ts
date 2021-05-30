80d5aeeb509e9ae4fc74a3a8667be552e30df6cd
Felipe Marinho <felipmarinho@gmail.com>
2021-05-29 21:02:19 -0300

    refactor categoriesRoutes to use factory

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,14 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../models/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../models/cars/services/CreateCategoryService";
+import {
+  createCategoryController,
+  categoriesRepository,
+} from "../models/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  createCategoryService.execute({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {

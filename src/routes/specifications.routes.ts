d24a11b48e0df212c1ec89057da8f4d116d0e8d5
Felipe Marinho <felipmarinho@gmail.com>
2021-05-30 07:05:29 -0300

    use factory of Specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,18 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationRepository } from "../models/cars/repositories/implementations/SpecificationRepository";
-import { CreateSpecificationService } from "../models/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../models/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
-const specificationRepository = new SpecificationRepository();
 
 specificationsRoutes.post("/", (request, response) => {
-  const createSpecificationService = new CreateSpecificationService(
-    specificationRepository
-  );
-  const { name, description } = request.body;
-  createSpecificationService.execute({ name, description });
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 });
 
 export { specificationsRoutes };

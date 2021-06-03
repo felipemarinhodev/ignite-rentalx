a68c0f3685e06ba06bf2408ae37c8419e1b02707
Felipe Marinho <felipmarinho@gmail.com>
2021-06-03 19:40:41 -0300

    refactor path

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -4,8 +4,8 @@ import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
   constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
-  handle(request: Request, response: Response): Response {
-    const result = this.listCategoriesUseCase.execute();
+  async handle(request: Request, response: Response): Promise<Response> {
+    const result = await this.listCategoriesUseCase.execute();
     return response.json(result);
   }
 }

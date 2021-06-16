f908d7c6a05b60d5cac6babccf17152c81a6acb3
Felipe Marinho <felipmarinho@gmail.com>
2021-06-16 06:35:01 -0300

    test: add structure to CreateCategoryUseCase test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,7 @@
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {});

3a693e8ddb0b438660863a303a3265c6c17d1b02
Felipe Marinho <felipmarinho@gmail.com>
2021-06-16 06:36:14 -0300

    test: ensure to be able to create a new category

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -4,4 +4,26 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 let createCategoryUseCase: CreateCategoryUseCase;
 let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
 
-describe("Create Category", () => {});
+describe("Create Category", () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it("Should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category description Test",
+    };
+    await createCategoryUseCase.execute(category);
+
+    const categoryReceived = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    expect(categoryReceived).toHaveProperty("id");
+    expect(category.name).toBe(categoryReceived.name);
+  });
+});

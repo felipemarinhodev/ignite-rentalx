5154d19a02e70987c11fff2971371257cd9d29da
Felipe Marinho <felipmarinho@gmail.com>
2021-06-25 07:56:54 -0300

    feat: ensure will list only available cars

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -34,4 +34,18 @@ export class CarsRepositoryInMemory implements ICarsRepository {
   async findByLicensePlate(license_plate: string): Promise<Car> {
     return this.cars.find((car) => car.license_plate === license_plate);
   }
+
+  async findAvailable(
+    category_id?: string,
+    brand?: string,
+    name?: string
+  ): Promise<Car[]> {
+    return this.cars.filter(
+      (car) =>
+        car.available === true ||
+        (brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
+    );
+  }
 }

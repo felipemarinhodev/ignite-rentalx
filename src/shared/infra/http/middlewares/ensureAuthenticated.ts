387a31795e5fe2fd607b2ccafbe77bf89adfbf9d
Felipe Marinho <felipmarinho@gmail.com>
2021-06-25 06:58:43 -0300

    fix: adjustment for work

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -29,11 +29,11 @@ export async function ensureAuthenticated(
     const usersRepository = new UsersRepository();
     const user = await usersRepository.findById(user_id);
 
-    request.user = { id: user_id };
-
     if (!user) {
       throw new AppError("User does not exists!", 401);
     }
+
+    request.user = { id: user_id };
 
     next();
   } catch (error) {

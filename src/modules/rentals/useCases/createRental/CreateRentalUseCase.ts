d54af9c9dcb5e9af877a823f0d50b9b9211cba97
Felipe Marinho <felipmarinho@gmail.com>
2021-07-13 06:41:23 -0300

    create the use Case of CreateRentaUseCase

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,52 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { AppError } from "@shared/errors/AppError";
+
+import { IRentalsRepository } from "../../repositories/IRentalsRepository";
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+// @injectable()
+class CreateRentalUseCase {
+  constructor(
+    // @inject("")
+    private rentalsRepository: IRentalsRepository
+  ) {}
+
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+    // O aluguel deve ter duração mínima de 24 horas
+    // Não deve ser possível cadastrar um novo alugel caso já exista um aberto para o mesmo usuário
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+    if (rentalOpenToUser) {
+      throw new AppError("There is a rental in progress for the user!");
+    }
+
+    // Não deve ser possível cadastrar um novo alugel caso já exista um aberto para o mesmo carro
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is unavailable");
+    }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };

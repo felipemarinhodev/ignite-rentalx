d6f43af9cbf3e9ce0b2ac55210e6ba01e9b3ac59
Felipe Marinho <felipmarinho@gmail.com>
2021-07-13 06:37:34 -0300

    add Rental entity

---
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -0,0 +1,29 @@
+// import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidV4 } from "uuid";
+
+// @Entity('')
+class Rental {
+  // @PrimaryColumn()
+  id: string;
+
+  // @Column()
+  car_id: string;
+
+  user_id: string;
+
+  // @CreateDateColumn()
+  start_date: Date;
+  end_date: Date;
+  expected_return_date: Date;
+  total: number;
+  created_at: Date;
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Rental };

bd09c6b2f18d91fe22de8f4a832264eca6267c33
Felipe Marinho <felipmarinho@gmail.com>
2021-07-17 09:04:50 -0300

    mapping the entity

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,22 +1,28 @@
-// import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
-// @Entity('')
+@Entity("rentals")
 class Rental {
-  // @PrimaryColumn()
+  @PrimaryColumn()
   id: string;
 
-  // @Column()
+  @Column()
   car_id: string;
 
+  @Column()
   user_id: string;
 
-  // @CreateDateColumn()
+  @Column()
   start_date: Date;
+  @Column()
   end_date: Date;
+  @Column()
   expected_return_date: Date;
+  @Column()
   total: number;
+  @CreateDateColumn()
   created_at: Date;
+  @CreateDateColumn()
   updated_at: Date;
 
   constructor() {
